a05a6e51cf4baf305386a5def780e474c42c7a5f
Alfianyus <alfian.yuska1@gmail.com>
2024-02-26 17:42:34 +0700

    updateenv

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,45 +1,86 @@
-const getAllUsers =(req, res) =>{
-    const data ={
-        id:'1',
-        name:"alfian",
-        email:"alfian@gmail.com",
-        address:"sawangan"
+const UsersModel = require('../models/users');
+
+const getAllUsers = async (req, res) =>{
+
+    try {
+        const [data] = await UsersModel.getAllUsers();
+    
+        res.json({
+            message:'GET all users success',
+            data:data
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage:error,
+        })
+        
+    }
+    
+}
+
+const createNewUsers = async (req, res) => {
+    const {body} = req;
+
+    if(!body.name || !body.email || !body.address){
+        return res.status(400).json({
+            message:"anda mungkin memasukan data yang salah",
+            data: null
+        })
     }
 
-    res.json({
-        message:'GET all users success',
-        data:data
-    })
+    try {
+        await UsersModel.createNewUser(body);
+        res.status(201).json({
+            message: 'CREATE new users success',
+            data: body
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage:error,
+        })
+    }
 }
 
-const createNewUsers = (req, res) =>{
-    console.log(req.body);
-    res.json({
-        message: 'CREATE new users success',
-        data: req.body
-    })
-}
-
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
     const{idUser}=req.params;
-    console.log('idUser:',idUser);
-    res.json({
-        message: 'Update user success',
-        data: req.body,
-    })
+    const {body} = req;
+    try {
+        await UsersModel.updateUser(body, idUser);
+        res.json({
+            message: 'Update user success',
+            data: {
+                id: idUser,
+                ...body
+            },
+        })        
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage:error,
+        })
+        
+    }
 }
 
-const deleteUser =(req, res) => {
+const deleteUser = async (req, res) => {
     const {idUser}=req.params;
-    res.json({
-        message:"Delete user success",
-        data:{
-            id:idUser,
-            name:"alfian",
-            email:"alfian@gmail.com",
-            address:"sawangan-depok"
-        }
-    })
+    try {
+        await UsersModel.deleteUser(idUser);
+        res.json({
+            message:"Delete user success",
+            data: null
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage:error,
+        })
+        
+    }
 }
 
 module.exports ={
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
+require('dotenv').config()
+
+const PORT = process.env.PORT;
+
 const express = require('express');
-
-const mysql = require('mysql2');
-
 
 const usersRoutes = require('./routes/users');
 
 const middlewareLogRequest = require('./middleware/logs');
 
-const dbPool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password:'',
-    database: 'express_mysql',
-    port: 4306,
-  });
+
 
 const app = express();
 
@@ -23,24 +18,6 @@ app.use(express.json());
 
 app.use('/users', usersRoutes);
 
-app.use('/', (req, res) => {
-    dbPool.execute('SELECT * FROM users', (err, rows) => {
-        if(err){
-           return res.json({
-                message: 'connection failed',
-                error: err,
-            })
-        }
-
-       return res.json({
-            message:'connection success',
-            data: rows,
-        })
-    })
-} )
-
-
-
-app.listen(4000, () =>{
-    console.log('Server berhasil di running di port 4000');
+app.listen(PORT, () =>{
+    console.log(`Server berhasil di running di port ${PORT}`);
 })
